f23013a177b06041ec8b77a61e181e9d72f15fe1
Jack Aitken <jaitk11@gmail.com>
2022-01-10 17:57:19 -0500

    Changing array size for testing

--- src/lib/getRandomArray.js
+++ src/lib/getRandomArray.js
@@ -3,7 +3,7 @@ function getRandomArray() {
   let min = 1;
   let max = 100;
 
-  for (let i = 0; i < 20; i++) {
+  for (let i = 0; i < 100; i++) {
     let randNum = Math.floor(Math.random() * (max - min) + min);
     randomizedArray.push(randNum);
   }
