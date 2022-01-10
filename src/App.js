d296a6a3d50ec2e39bee8e5f5b271ca40a09e697
Jack Aitken <jaitk11@gmail.com>
2022-01-09 23:04:57 -0500

    Bubble sort visualization. Needs work though

--- src/App.js
+++ src/App.js
@@ -1,32 +1,54 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { getRandomArray } from './lib/getRandomArray'
 import ArrayRepresentation from './ArrayRepresentation';
 import SetNewArrayButton from './SetNewArrayButton';
 import BubbleSortButton from './BubbleSortButton';
-import { bubbleSort } from './lib/algorithms/bubbleSort';
-import { quicksort } from './lib/algorithms/quicksort';
-import { insertionSort } from './lib/algorithms/insertionSort';
-import { mergeSort } from './lib/algorithms/mergeSort';
+import { SortingAlgorithms } from './lib/algorithms/sortingAlgorithms';
 import styles from './styles/AppStyles.module.css';
 import { compareSort } from './lib/algorithms/testSort';
 
 function App() {
   const [ array, setArray ] = useState(getRandomArray());
+  const [ animationArr, setAnimationArr ] = useState();
+  // const [ algorithm, setAlgorithm ] = useState(SortingAlgorithms.algorithms['bubble']);
+  const animationIsSet = useRef(false);
+
+
+  function handleBubbleSortAnimation(sortAlgorithm) { 
+    setAnimationArr(sortAlgorithm([...array]));
+  }
 
   useEffect(() => {
-    console.log('Dom has changed');
-  });
+    if (animationIsSet.current) {
+      for (let i = 0; i < animationArr.length; i++) {
+        setTimeout(() => {
+          let [ indexOne, indexTwo ] = animationArr[i].startState;
+          let firstBarStyle = document.getElementById(`arrayBar${indexOne}`).style;
+          let secondBarStyle = document.getElementById(`arrayBar${indexTwo}`).style;
+
+          firstBarStyle.background = 'red';
+          secondBarStyle.background = 'red';
+
+          if (animationArr[i].swap) {
+            let tempHeight = firstBarStyle.height
+            firstBarStyle.height = secondBarStyle.height
+            secondBarStyle.height = tempHeight;
+          }
+
+          firstBarStyle.background = 'black';
+        }, i * 10);
+      }
+    } else {
+      animationIsSet.current = true;
+    }
+  }, [animationArr]);
 
   return (
     <>
       <div className={styles.buttonWrapper}>
         <SetNewArrayButton handleClick={() => setArray(getRandomArray())}/>
-        <BubbleSortButton handleClick={() => setArray(bubbleSort([...array]))} />
-        {/* <button className={styles.algoButton} onClick={() => setArray(bubbleSort(array))}>Bubble Sort</button> */}
-        {/* <button className={styles.algoButton} onClick={() => compareSort(array, algorithms['quick'])}>Quicksort</button>
-        <button className={styles.algoButton} onClick={() => compareSort(array, algorithms['insertion'])}>Insertion Sort</button>
-        <button className={styles.algoButton} onClick={() => compareSort(array, algorithms['merge'])}>Merge Sort</button> */}
-      </div>
+        <BubbleSortButton handleClick={() => handleBubbleSortAnimation(SortingAlgorithms.bubbleSort)} />
+      </div>      
       <div>
         <ArrayRepresentation array={array} />
       </div>
@@ -44,11 +66,4 @@ Testing buttons
 <button className={styles.algoButton} onClick={() => compareSort(array, algorithms['quick'])}>Quicksort</button>
 <button className={styles.algoButton} onClick={() => compareSort(array, algorithms['insertion'])}>Insertion Sort</button>
 <button className={styles.algoButton} onClick={() => compareSort(array, algorithms['merge'])}>Merge Sort</button>
-
-  const algorithms = {
-    'bubble': bubbleSort,
-    'quick': quicksort,
-    'insertion': insertionSort,
-    'merge': mergeSort,
-  }
 */
