e7b7018a235723d15002bf339b9fe604d0f4a086
Jack Aitken <jaitk11@gmail.com>
2022-01-10 17:58:03 -0500

    Made bubble sort more efficient

--- src/lib/algorithms/sortingAlgorithms.js
+++ src/lib/algorithms/sortingAlgorithms.js
@@ -9,24 +9,29 @@ class SortingAlgorithms {
   static bubbleSort(array) {
     let animationArr = [];
     let currentlyUnsorted = array.length - 1;
+    let sorted = false;
   
-    for (let i = 0; i < currentlyUnsorted; i++) {
-      for (let j = 0; j < currentlyUnsorted; j++) {
+    while (!sorted) {
+      sorted = true;
+      for (let i = 0; i < currentlyUnsorted; i++) {
         let animationObj = {};
-        animationObj.startState = [j, j + 1];
-
-        if (array[j] > array[j + 1]) {
-            let tmp = array[j];
-            array[j] = array[j + 1];
-            array[j + 1] = tmp;
-            animationObj.swap = true;
-          } else {
-            animationObj.swap = false;
-          }
-          animationArr.push(animationObj);
+        animationObj.startState = [i, i + 1];
+        if (array[i] > array[i + 1]) {
+          animationObj.swap = true;
+          [ array[i], array[i + 1] ] = [ array[i + 1], array[i] ];
+          sorted = false;
+        } else {
+          animationObj.swap = false;
         }
+        animationArr.push(animationObj);
+      }
+      currentlyUnsorted -= 1;
     }
     return animationArr;
+  }
+
+  static bubbleSortTwo(array) {
+
   }
 
   static quicksort() {
